e4f36dd80cfb49004b40acdc0cb480e067a1f26a
Duncan <DuncanKushnir@users.noreply.github.com>
2018-04-13 23:52:00 +0200

    Minor updates and tidying

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -5,9 +5,10 @@ session=pySafe.connection.Connection()
 #todo better ways to get key/pass
 import getpass
 
-username = getpass.getpass()
-key = getpass.getpass()
+# username = getpass.getpass()
+# key = getpass.getpass()
 
-
+username = 'foo'
+key = 'bar'
 
 session.login(username,key)
